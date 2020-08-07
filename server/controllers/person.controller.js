1bdc2832b0678f2e83b01ec57e2bb76dd1ed7be4
jo <jay.jo.dev@gmail.com>
2020-08-07 17:54:08 +0900

    add CRUD for person[server]

--- server/controllers/person.controller.js
+++ server/controllers/person.controller.js
@@ -1,11 +1,15 @@
 const Person = require("../models/Person");
 
 module.exports = (app) => {
+
+  // Get All People
   app.get("/api/person", async (req, res) => {
     const people = await Person.find({});
     res.send(people);
   });
 
+
+  // Get Add People
   app.post("/api/person", async (req, res) => {
     const { name } = req.body;
     const person = new Person({
@@ -17,5 +21,26 @@ module.exports = (app) => {
     } catch (err) {
       res.send(400, err);
     }
+  });
+
+  
+  // Update person`s name
+  app.put("/api/person/:id", async (req, res) => {
+    await Person.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
+      if (err) {
+        return res.status(500).send({ error: "unsuccessful" });
+      }
+      res.send({ success: "success" });
+    });
+  });
+
+  // Delete person`s name
+  app.delete("/api/person/:id", async (req, res) => {
+    await Person.deleteOne({ _id: req.params.id }, (err, user) => {
+      if (err) {
+        return res.status(500).send({ error: "unsuccessful" });
+      }
+      res.send({ success: "success" });
+    });
   });
 };
